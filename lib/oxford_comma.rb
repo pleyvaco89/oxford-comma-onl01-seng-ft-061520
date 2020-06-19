baba2f62a1ec7cd3ffeafdf2877693fcbd6bacf8
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-19 07:03:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(array)
-  if array.length == 1
-    return "#{array[0]}"
-  elsif array.length == 2
-    return array.join(" and ")
-  elsif array.length >= 3
-    array[-1] = "and #{array[-1]}"
-    return array.join(", ")
+  if array.size < 3
+    array.join(' and ')
+  else
+    array[-1] = 'and ' + array[-1]
+    array.join(', ')
   end
 end
